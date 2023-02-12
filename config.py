5069106b31a60cd11999a7b37d1ece02862cc770
Hlib <gggleb13@gmail.com>
2023-02-12 15:09:00 +0200

    Ready project

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN_API = "5813791681:AAGvX61WbSyp46GtWonLvncRVYfW1GMw3c4"
+
+CHANNELS = [
+    ("Дом-грез", "-1001600481277", "https://t.me/movie_dreamhouse")
+]
+
+NOT_SUB_MESSAGE = "Для доступа к базе данных фильмов подпишитесь на каналы⬇️⬇️⬇️"
+
+#panda 5813791681:AAGvX61WbSyp46GtWonLvncRVYfW1GMw3c4
+#test bot 5463406982:AAEuFteg7zJ2cr1TucaZ-LmQ0feILoF3dpk
+#("Brazuca News", "-1001855624999", "https://t.me/brazucanews")
